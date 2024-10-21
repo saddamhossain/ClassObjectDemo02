e9c2ca3bc6fa1b94d1597b380416d971336144cb
saddamhossain <saddamhossain953@gmail.com>
2024-10-22 00:34:31 +0600

    Add project files.

---
+++ ClassObjectDemo02/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassObjectDemo02;
+
+public class BankAccount
+{
+    public string AccountNumber { get; private set; }
+    public string AccountName { get; private set; }
+    public double Balance { get; private set; }
+
+    public BankAccount(string accountNumber, string accountName, double initialBalance)
+    {
+        AccountNumber = accountNumber;
+        AccountName = accountName;
+        Balance = initialBalance >= 0 ? initialBalance : 0;
+    }
+
+    public bool Deposite(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Withdraw(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Transfer(double amount, BankAccount toAccount)
+    {
+        if (amount > 0)
+        {
+            Balance -= amount;
+            toAccount.Deposite(amount);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ ClassObjectDemo02/Program.cs
@@ -0,0 +1,41 @@
+﻿BankAccount bankAccount1 = new BankAccount("ACC-001", "Md. Saddam Hossain", 1000);
+
+BankAccount bankAccount2 = new BankAccount("ACC-002", "Rana", 500);
+
+Console.WriteLine($"{bankAccount1.AccountName}'s initial balance: {bankAccount1.Balance}");
+
+Console.WriteLine($"{bankAccount2.AccountName}'s initial balance: {bankAccount2.Balance}");
+
+if (bankAccount1.Deposite(100))
+{
+    Console.WriteLine($"Deposited to {bankAccount1.AccountName}'s account. New balance: {bankAccount1.Balance}");
+}
+else
+{
+    Console.WriteLine("Failed to deposit.");
+}
+
+if (bankAccount1.Withdraw(50))
+{
+    Console.WriteLine($"Withdraw from {bankAccount1.AccountName}'s account. New balance: {bankAccount1.Balance}");
+}
+else
+{
+    Console.WriteLine("Failed to withdraw. Insufficient funds or invalid amount.");
+}
+
+
+if (bankAccount1.Transfer(200, bankAccount2))
+{
+    Console.WriteLine($"Transferred to {bankAccount2.AccountName}'s account. New balance of {bankAccount1.AccountName}'s : {bankAccount1.Balance}");
+
+    Console.WriteLine($"{bankAccount2.AccountName}'s new balance: {bankAccount2.Balance}");
+}
+else
+{
+    Console.WriteLine("Failed to transfer. Insufficient funds or invalid amount.");
+}
+
+Console.WriteLine($"{bankAccount1.AccountName}'s final balance: {bankAccount1.Balance}");
+
+Console.WriteLine($"{bankAccount2.AccountName}'s final balance: {bankAccount2.Balance}");
